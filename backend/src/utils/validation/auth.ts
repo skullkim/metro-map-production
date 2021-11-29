fb96e1daaca341926b1751de3f0a661da2ae7aa5
skullKim <kyk990328@gmail.com>
2021-11-29 21:15:35 +0900

    수정: 백엔드 코드 중 비밀번호 검증하는 정규식에 특수문자 '^'가 생략되있는 문제 해결

--- backend/src/utils/validation/auth.ts
+++ backend/src/utils/validation/auth.ts
@@ -1,6 +1,6 @@
 export const isValidPassword = (password: string): boolean => {
   const passwordRegexp: RegExp =
-    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/;
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%^*#?&])[A-Za-z\d$@$!%^*#?&]{8,}$/;
   return passwordRegexp.test(password);
 };
 
